5b90b1fa7c7f7e11d5cfa786c8d8d75c608840a3
xaviervalles <xaviervallesr@iqs.url.edu>
2020-06-03 13:20:24 +0200

    Grafico de Barras sobre los incidentes en los barrios

--- First dana analysis.R
+++ First dana analysis.R
@@ -1,2 +1,12 @@
 3+3
 codetools::checkUsage()
+
+library(ggplot2)
+
+NYPD<-read.csv('NYPD_Shooting_Incident_Data__Historic.csv')
+
+summary(NYPD)
+
+(BAR_Boro<-ggplot(data=NYPD,aes(x=BORO))+
+    geom_bar(stat='count'))
+BAR_Boro
