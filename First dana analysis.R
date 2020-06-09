52fcc181ed6bf2c290bb73f7b691f939887eaf7a
albertpj97 <albertperezjane@gmail.com>
2020-06-09 12:36:05 +0200

    sin canvios

--- First dana analysis.R
+++ First dana analysis.R
@@ -1,3 +1,15 @@
-3+3
-codetools::checkUsage()
- ghv
+
+<<<<<<< HEAD
+
+=======
+
+library(ggplot2)
+
+NYPD<-read.csv('NYPD_Shooting_Incident_Data__Historic.csv')
+
+summary(NYPD)
+
+(BAR_Boro<-ggplot(data=NYPD,aes(x=BORO))+
+    geom_bar(stat='count'))
+BAR_Boro
+>>>>>>> adcb2d50f67b8dc0129d2d4114e04d6ec42abca7
